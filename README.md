8d26519943998fa3448aea45d6a998efe6ea5b87
dymafr <erwan1309@hotmail.com>
2022-02-22 12:28:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-vyuwae
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-vyuwae)
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <label for="quantity">Nombre de livres : </label>
+    <input id="quantity" type="number" v-model="produit.quantite" />
+  </div>
+
+  <div>
+    <label for="prix">Prix : </label
+    ><input id="prix" type="number" v-model="produit.prix" />
+  </div>
+  <h2>Prix total HT : {{ totalPrixHT }}€</h2>
+  <h2>Prix total TTC : {{ totalPrixTTC }}€</h2>
+  <h3>Modifications : {{ produit.nbrModifs }}</h3>
+</template>
+
+<script setup lang="ts">
+import { computed, reactive, watch } from 'vue';
+
+const produit = reactive({
+  quantite: 3,
+  prix: 10,
+  nom: 'livre',
+  nbrModifs: 0,
+});
+
+const totalPrixHT = computed(() => produit.quantite * produit.prix);
+const totalPrixTTC = computed(() => produit.quantite * produit.prix * 1.2);
+
+watch(
+  () => [produit.quantite, produit.prix],
+  (nouvelleVal, ancienneVal) => {
+    console.log(nouvelleVal, ancienneVal);
+    produit.nbrModifs++;
+  }
+);
+</script>
+
+<style></style>
