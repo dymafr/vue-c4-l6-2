544215e12adf21716a6f926f7227f2c011c6f686
dymafr <erwan1309@hotmail.com>
2022-02-22 12:29:54 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,29 @@
 <template>
-  <div>
-    <label for="quantity">Nombre de livres : </label>
-    <input id="quantity" type="number" v-model="produit.quantite" />
-  </div>
-
-  <div>
-    <label for="prix">Prix : </label
-    ><input id="prix" type="number" v-model="produit.prix" />
-  </div>
-  <h2>Prix total HT : {{ totalPrixHT }}€</h2>
-  <h2>Prix total TTC : {{ totalPrixTTC }}€</h2>
-  <h3>Modifications : {{ produit.nbrModifs }}</h3>
+  <p>
+    Ask a yes/no question:
+    <input v-model="question" />
+  </p>
+  <p>{{ answer }}</p>
 </template>
 
 <script setup lang="ts">
 import { computed, reactive, watch } from 'vue';
 
-const produit = reactive({
-  quantite: 3,
-  prix: 10,
-  nom: 'livre',
-  nbrModifs: 0,
-});
+const question = ref('');
+const answer = ref('Questions usually contain a question mark. ;-)');
 
-const totalPrixHT = computed(() => produit.quantite * produit.prix);
-const totalPrixTTC = computed(() => produit.quantite * produit.prix * 1.2);
-
-watch(
-  () => [produit.quantite, produit.prix],
-  (nouvelleVal, ancienneVal) => {
-    console.log(nouvelleVal, ancienneVal);
-    produit.nbrModifs++;
+// watch works directly on a ref
+watch(question, async (newQuestion, oldQuestion) => {
+  if (newQuestion.indexOf('?') > -1) {
+    answer.value = 'Thinking...';
+    try {
+      const res = await fetch('https://yesno.wtf/api');
+      answer.value = (await res.json()).answer;
+    } catch (error) {
+      answer.value = 'Error! Could not reach the API. ' + error;
+    }
   }
-);
+});
 </script>
 
 <style></style>
